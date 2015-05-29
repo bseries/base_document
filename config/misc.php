0242af0141ae1d73ba8c69c0c2e2ee209988d5c3
David Persson <davidpersson@gmx.de>
2015-05-29 11:00:22 +0200

    Initial

---
+++ config/misc.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Base Document
+ *
+ * Copyright (c) 2015 Atelier Disko - All rights reserved.
+ *
+ * This software is proprietary and confidential. Redistribution
+ * not permitted. Unless required by applicable law or agreed to
+ * in writing, software distributed on an "AS IS" BASIS, WITHOUT-
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+use lithium\core\Libraries;
+
+// Enable document classes and paths via i.e. Libraries::locate('document') and
+// Libraries::locate('document.billing').
+Libraries::paths([
+	'document' => [
+		'{:library}\extensions\document\{:class}\{:name}',
+		'{:library}\extensions\document\{:name}'
+	]
+]);
+
+?>
