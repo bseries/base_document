6abc070160291f95d1ef1ae5d58d932a1c1d4089
David Persson <davidpersson@gmx.de>
2015-05-29 16:44:22 +0200

    Add shipment doc.

---
+++ document/Shipment.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Base Document
+ *
+ * Copyright (c) 2015 Atelier Disko - All rights reserved.
+ *
+ * This software is proprietary and confidential. Redistribution
+ * not permitted. Unless required by applicable law or agreed to
+ * in writing, software distributed on an "AS IS" BASIS, WITHOUT-
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+namespace base_document\document;
+
+class Shipment extends \base_document\document\Invoice {}
+
+?>
